f2770944d1f7987e8bb20f61e2681f68c0e3bc74
harsh24062 <harshlol007@gamil.com>
2025-01-07 19:09:10 +0530

    implementing WebSocket

--- src/main/java/com/chatter/chatter_backend/config/WebSocketConfig.java
+++ src/main/java/com/chatter/chatter_backend/config/WebSocketConfig.java
@@ -1,8 +1,11 @@
 package com.chatter.chatter_backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-public class WebSocketConfig {
-
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+   
 }
